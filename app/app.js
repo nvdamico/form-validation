39df653fdd058e5cf4c7eb2397baf60065502a2a
Nisha D'Amico <nisha.damico@opower.com>
2016-07-25 12:19:56 -0400

    some validation

--- app/app.js
+++ app/app.js
@@ -5,6 +5,18 @@ var validationApp = angular.module('validationApp', []);
 // create angular controller
 validationApp.controller('mainController', function($scope) {
 
+    $scope.user = {};
+    $scope.ssnNumbers = /^\d+$/;
+
+    $scope.confirmEmailMatch = function() {
+        return $scope.user.email === $scope.user.emailConfirm;
+    };
+
+    $scope.isValidSSN = function() {
+        console.log('calling this');
+        return $scope.user.ssn !== 123456789 && $scope.user.ssn !== 987654321;
+    }
+
   // function to submit the form after all validation has occurred            
   $scope.submitForm = function(isValid) {
 
